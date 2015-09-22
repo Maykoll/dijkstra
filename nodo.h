bd8295ecf08be01113380134746946374ef2986d
Maykoll <maycol.gonzalez7@gmail.com>
2015-09-21 23:35:01 -0400

    Update nodo.h

--- nodo.h
+++ nodo.h
@@ -1,1 +1,19 @@
+#ifndef stdio_h
+#include <stdio.h>
+#define stdio_h
+#endif
+
+#ifndef stdlib_h
+#include <stdlib.h>
+#define stdlib_h
+#endif
+
+typedef struct tipo_nodo{	
+		
+		char Nombre ;   //Nombre del vertice A,B...etc
+		int peso;
+		int posicion;
+		int color;
+							
+	}Nodo;
 
