8c0ef2cce0c179565ef2e348aa1e136c70893d1e
Maykoll <maycol.gonzalez7@gmail.com>
2015-09-22 21:58:11 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
+#ifndef stdio_h
+#include <stdio.h>
+#define stdio_h
+#endif
 
+#ifndef stdlib_h
+#include <stdlib.h>
+#define stdlib_h
+#endif
+
+#ifndef grafo_h
+#include "grafo.h"
+#define grafo_h
+#endif
+
+#ifndef nodo_h
+#include "nodo.h"
+#define nodo_h
+#endif
+
+
+ main(){
+ 	
+	return 0;
+}
